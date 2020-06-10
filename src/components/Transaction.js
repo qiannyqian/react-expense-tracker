b16beac96fe716708bdc9414f9cddbdd079f0b73
qiannyqian <liang.huiqian@gmail.com>
2020-06-10 15:08:44 +0800

    Enable GlobalContext for IncomeExpenses component

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -4,10 +4,10 @@ export const Transaction = ({ transaction }) => {
   const sign = transaction.amount < 0 ? '-' : '+';
   return (
     <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
-      {transaction.text}{' '}
+      {transaction.text}
       <span>
         {sign}${Math.abs(transaction.amount)}
-      </span>{' '}
+      </span>
       <button className="delete-btn">x</button>
     </li>
   );
